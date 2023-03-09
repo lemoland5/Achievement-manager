ba4edb9f73ce8f903748b8f09b56e8dc714e4484
unknown <lemo.cam.channel@gmail.com>
2023-03-10 00:04:33 +0100

    added basic mongodb support

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
@@ -8,19 +9,53 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public/html/index.html'))
-})
+main().catch(err => console.log(err));
 
-app.post('/', (req, res)=>{
-    console.log(JSON.stringify(req.body));
-    res.sendFile(path.join(__dirname, 'public/html/index.html'))
-})
+async function main() {
 
-app.get('/form', (req, res)=>{
-    res.sendFile(path.join(__dirname, 'public/html/form.html'))
-})
+    //DATABASE
 
-app.listen(port, ()=>{
-    console.log(`Server listening on port ${port}..`);
-})
+    await mongoose.connect('mongodb://127.0.0.1:27017/ach');
+
+    const achSchema = new mongoose.Schema({
+        name: String,
+        description: String,
+        rare: Boolean
+    });
+
+    const Achievement = mongoose.model('Achievement', achSchema);
+
+    const seamoth = new Achievement({name: `Personal Propulsion`, description:`Build a Seamoth`, rare: false})
+
+    await seamoth.save();
+
+    //SERVER
+
+    app.get('/', (req, res)=>{
+        res.status(200);
+        res.sendFile(path.join(__dirname, 'public/html/index.html'));
+        console.log(`${req.ip} connected to '/' using GET`);
+    })
+    
+    app.post('/', (req, res)=>{
+        res.status(200);
+
+        //res.sendFile(path.join(__dirname, 'public/html/index.html'));
+
+        res.send(seamoth.name);
+
+
+        console.log(`${req.ip} connected to '/' using POST`);
+    })
+    
+    app.get('/admin', (req, res)=>{
+        res.status(200);
+        res.sendFile(path.join(__dirname, 'public/html/form.html'));
+        console.log(`${req.ip} connected to '/form' using GET`);
+    })
+    
+    app.listen(port, ()=>{
+        console.log(`Server listening on port ${port}..`);
+    })
+
+}
